3eb156beddc072e091d4589ac4b0ae311a038534
Alex Henry <adhe223@g.uky.edu>
2018-10-29 20:41:43 -0700

    Start the move to custom classes to prevent dom pollution with the injected scripts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const prepareDOM = () => {
-  resetPositionTabsWidth();
-  resetStatusTabsWidth();
+  preparePositionTabs();
+  prepareStatusTabs();
+  prepareTableBody();
 };
 
 const getPositionTabs = () => {
@@ -11,19 +12,34 @@ const getStatusTabs = () => {
   return document.querySelectorAll('.Table2__td--fixed-width:nth-child(5)');
 };
 
-const resetPositionTabsWidth = () => {
+const getTableBody = () => {
+  return document.querySelector('.Table2__table__wrapper tbody:nth-child(1)');
+};
+
+const preparePositionTabs = () => {
   const tabs = getPositionTabs();
   resetElementsInlineWidth(tabs);
 };
 
-const resetStatusTabsWidth = () => {
+const prepareStatusTabs = () => {
   const tabs = getStatusTabs();
   resetElementsInlineWidth(tabs);
+};
+
+const prepareTableBody = () => {
+  const tableBody = getTableBody();
+  addCustomClassToElements('custom-TableBody', [tableBody]);
 };
 
 const resetElementsInlineWidth = elements => {
   elements.forEach(element => {
     element.style.width = null;
+  });
+};
+
+const addCustomClassToElements = (className, elements) => {
+  elements.forEach(element => {
+    element.classList.add(className);
   });
 };
 
